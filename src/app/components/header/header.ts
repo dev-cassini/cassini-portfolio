6ae74711b07ac4728bb0f9fbcd5f9bf89dd239b1
ryandremza <ryan.dremza@gmail.com>
2025-11-29 11:37:43 +0000

    add logo to header and link back to home page on click

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, ThemeToggle],
+  imports: [RouterLink, ThemeToggle, LogoComponent],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  constructor(private router: Router) { }
 
+  navigateToHome() {
+    this.router.navigate(['/']);
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
